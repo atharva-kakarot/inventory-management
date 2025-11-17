a11a47b12a8bdb29927926630f8ea76c42170ea6
atharva-kakarot <atharva.kakarot@gmail.com>
2025-11-17 16:12:41 +0530

    Added tailwind.config.ts file

---
+++ client/tailwind.config.ts
@@ -0,0 +1,35 @@
+import type { Config } from "tailwindcss";
+import { createThemes } from "tw-colors";
+
+const baseColors = [
+  "gray",
+  "red",
+  "yellow",
+  "green",
+  "blue",
+  "indigo",
+  "purple",
+  "pink",
+];
+
+const shadeMapping = {
+    "50": "100",
+}
+
+const config: Config = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundColor: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
